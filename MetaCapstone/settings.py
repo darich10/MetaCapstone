034eb3014a43f5554479435f360a505003ef1979
Dario Rosas <dariorosas.ing@gmail.com>
2023-01-14 00:56:49 -0600

    models BookTable and Menu created

--- MetaCapstone/settings.py
+++ MetaCapstone/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'MetaCapstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '33060',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
